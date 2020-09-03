e842e04b20a1a80ec88f569ec11d91792956f6c9
Surya Teja Chavali <60577050+SuryaTeja2121@users.noreply.github.com>
2020-09-04 02:48:23 +0530

    Initial Commit of Java

---
+++ ThisIsMyFirstRepository.java
@@ -0,0 +1,8 @@
+public class ThisIsMyFirstRepository {
+
+        public static void main(String[] args) {
+
+                 System.out.println(" This is my First Repository in GitHub");
+
+        }
+}
